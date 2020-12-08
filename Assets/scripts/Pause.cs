31770c05b99e6619f647aa4ea0621c507ad7655e
unhappybreadroll <samdoherty007@gmail.com>
2020-12-08 13:02:38 +0000

    added title

---
+++ Assets/scripts/Pause.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    [SerializeField] GameObject pauseMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+               if (isGamePaused)
+            {
+                ResumeGame();
+            }
+               else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+        public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+   public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMenue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Quit");
+    }
+
+
+
+}
---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+
+}
